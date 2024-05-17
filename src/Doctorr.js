f58377ec1b8cebda10c0d46bc7e9bfb8dd56902f
chdipak <dipaksunny009@gmail.com>
2024-05-17 17:22:21 +0530

    first commit

---
+++ src/Doctorr.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+const Doctorr = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    //GET
+    const fetchDoctors = async () => {
+      try{
+        const response = await axios.get('https://backendhospital-ji3g.onrender.com/doctors');
+        setDoctors(response.data);
+
+      } catch (error) {
+        console.error('Error fetching doctors:', error);
+      }
+    };fetchDoctors();
+  },[]);
+  return (
+    <div>
+      <center>
+      <h1>Doctor</h1>
+      {
+        doctors.map(doctor => (
+          <div key={doctor.id}>
+            <p><strong>{doctor.name}</strong> - {doctor.specialization}</p>
+            <p>Doctor Id: {doctor.id}</p>
+            </div>
+        ))
+      }
+      </center>
+    </div>
+  )
+}
+
+export default Doctorr;
